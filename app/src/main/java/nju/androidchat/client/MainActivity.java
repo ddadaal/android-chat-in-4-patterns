53bf6faa4db1c07c61ec597a40c1995cee60d4b8
张凌哲 <445073309@qq.com>
2019-05-10 18:52:10 +0800

    may complete my part

--- app/src/main/java/nju/androidchat/client/MainActivity.java
+++ app/src/main/java/nju/androidchat/client/MainActivity.java
@@ -1,10 +1,14 @@
 package nju.androidchat.client;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.core.view.GravityCompat;
@@ -20,19 +24,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import nju.androidchat.client.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView editText = findViewById(R.id.et_content);
+        editText.setImeOptions(EditorInfo.IME_ACTION_SEND);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                //当actionId == XX_SEND 或者 XX_DONE时都触发
+                //或者event.getKeyCode == ENTER 且 event.getAction == ACTION_DOWN时也触发
+                //注意，这是一定要判断event != null。因为在某些输入法上会返回null。
+                if (actionId == EditorInfo.IME_ACTION_SEND
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || (event != null && KeyEvent.KEYCODE_ENTER == event.getKeyCode() && KeyEvent.ACTION_DOWN == event.getAction())) {
+                    hideKeyboard();
+                    //处理事件
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
     }
 
+    @Override
+    // 点击空白位置 隐藏软键盘
+    public boolean onTouchEvent(MotionEvent event) {
+        if (null != this.getCurrentFocus()) {
+            return hideKeyboard();
+        }
+        return super.onTouchEvent(event);
+    }
+
+    //隐藏软键盘
+    private boolean hideKeyboard() {
+        InputMethodManager mInputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        return mInputMethodManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+    }
 }
